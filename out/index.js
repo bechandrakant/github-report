6f4e42604eb3690e7e8c0c22e69ec20c68fc463f
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-01-15 12:09:09 +0530

    command line argument feature added

--- out/index.js
+++ out/index.js
@@ -10,13 +10,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var GithubApiService_1 = require("./service/GithubApiService");
 var _ = __importStar(require("lodash"));
 var service = new GithubApiService_1.GithubApiService();
-var userName = "ckdetected";
-service.getUserInfo(userName, function (user) {
-    service.getRepos(userName, function (repos) {
-        var sortedRepo = _.sortBy(repos, function (repo) { return repo.size; });
-        var reverseSortedRepo = _.reverse(sortedRepo);
-        var topFive = _.take(reverseSortedRepo, 5);
-        user.repos = topFive;
-        console.log(user);
+if (process.argv.length < 3) {
+    console.log("Please pass username as an argument");
+}
+else {
+    var userName_1 = process.argv[2];
+    service.getUserInfo(userName_1, function (user) {
+        service.getRepos(userName_1, function (repos) {
+            var sortedRepo = _.sortBy(repos, function (repo) { return repo.size; });
+            var reverseSortedRepo = _.reverse(sortedRepo);
+            var topFive = _.take(reverseSortedRepo, 5);
+            user.repos = topFive;
+            console.log(user);
+        });
     });
-});
+}
