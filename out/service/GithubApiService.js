243f6f916e8493259e193555479e5425ef03c7c5
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-01-15 11:53:27 +0530

    working project done

--- out/service/GithubApiService.js
+++ out/service/GithubApiService.js
@@ -8,20 +8,31 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var request = __importStar(require("request"));
+var User_1 = require("../model/User");
+var Repo_1 = require("../model/Repo");
+var OPTIONS = {
+    headers: {
+        "User-Agent": "request"
+    },
+    json: true
+};
+var baseUrl = "https://api.github.com/users/";
 var GithubApiService = /** @class */ (function () {
     function GithubApiService() {
     }
-    GithubApiService.prototype.getUserInfo = function (userName) {
-        var options = {
-            headers: {
-                "User-Agent": "request"
-            }
-        };
-        request.get("https://api.github.com/users/" + userName, options, function (error, response, body) {
-            console.log(body);
+    GithubApiService.prototype.getUserInfo = function (userName, callbackFunction) {
+        request.get(baseUrl + userName, OPTIONS, function (error, response, body) {
+            var user = new User_1.User(body);
+            callbackFunction(user);
         });
     };
-    GithubApiService.prototype.getRepos = function () { };
+    GithubApiService.prototype.getRepos = function (userName, callbackFunc) {
+        request.get(baseUrl + userName + "/repos", OPTIONS, function (error, response, body) {
+            var repoArray = body;
+            var repos = repoArray.map(function (repo) { return new Repo_1.Repo(repo); });
+            callbackFunc(repos);
+        });
+    };
     return GithubApiService;
 }());
 exports.GithubApiService = GithubApiService;
--- src/service/GithubApiService.ts
+++ src/service/GithubApiService.ts
@@ -1,17 +1,30 @@
 import * as request from "request";
+import { User } from "../model/User";
+import { Repo } from "../model/Repo";
+
+const OPTIONS = {
+  headers: {
+    "User-Agent": "request"
+  },
+  json: true
+};
+
+const baseUrl = "https://api.github.com/users/";
 
 export class GithubApiService {
-  getUserInfo (userName: string) {
-    let options: any = {
-      headers: {
-        "User-Agent": "request"
-      }
-    }
-    request.get("https://api.github.com/users/" + userName, options, 
+  getUserInfo (userName: string, callbackFunction: (user: User) => any) {
+    request.get(baseUrl + userName, OPTIONS, 
                 (error: any, response: any, body: any) => {
-                  console.log(body);
+                  const user = new User(body);
+                  callbackFunction(user);
                 });
   }
 
-  getRepos() {}
+  getRepos(userName: string, callbackFunc: (repos: Repo[]) => any) {
+    request.get(baseUrl + userName + "/repos", OPTIONS, (error: any, response:any, body: any) => {
+      let repoArray = body;
+      let repos = repoArray.map ((repo: any) => new Repo(repo));
+      callbackFunc(repos);
+    });
+  }
 }
