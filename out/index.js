dd1a54a509c9e4c46516b60656ad5afd00e4b199
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-01-15 12:05:28 +0530

    chaining calls

--- out/index.js
+++ out/index.js
@@ -1,11 +1,22 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var GithubApiService_1 = require("./service/GithubApiService");
+var _ = __importStar(require("lodash"));
 var service = new GithubApiService_1.GithubApiService();
 var userName = "ckdetected";
 service.getUserInfo(userName, function (user) {
-    console.log(user);
-});
-service.getRepos(userName, function (repos) {
-    console.log(repos);
+    service.getRepos(userName, function (repos) {
+        var sortedRepo = _.sortBy(repos, function (repo) { return repo.size; });
+        var reverseSortedRepo = _.reverse(sortedRepo);
+        var topFive = _.take(reverseSortedRepo, 5);
+        user.repos = topFive;
+        console.log(user);
+    });
 });
