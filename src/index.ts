1fcd2632e16b6f12cbfa5f271a2cae943f379dd8
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-01-15 12:05:28 +0530

    chaining calls

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import {GithubApiService} from "./service/GithubApiService";
 import { User } from "./model/User";
 import { Repo } from "./model/Repo";
+import * as _ from "lodash";
 
 const service = new GithubApiService();
 const userName = "ckdetected";
 
 service.getUserInfo(userName, (user: User) => {
-  console.log(user);
+  service.getRepos(userName, (repos: Repo[]) => {
+    let sortedRepo = _.sortBy(repos, (repo: Repo) => repo.size);
+    let reverseSortedRepo = _.reverse(sortedRepo);
+    let topFive = _.take(reverseSortedRepo, 5);
+    user.repos = topFive;
+    console.log (user);
+  });
 });
 
-service.getRepos(userName, (repos: Repo[]) => {
-  console.log (repos);
-});
 
