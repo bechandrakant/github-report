21ac1465f4eb85cf13a2f20b155aa5a1272bdf9d
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-01-15 12:09:09 +0530

    command line argument feature added

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ import { Repo } from "./model/Repo";
 import * as _ from "lodash";
 
 const service = new GithubApiService();
-const userName = "ckdetected";
 
-service.getUserInfo(userName, (user: User) => {
-  service.getRepos(userName, (repos: Repo[]) => {
-    let sortedRepo = _.sortBy(repos, (repo: Repo) => repo.size);
-    let reverseSortedRepo = _.reverse(sortedRepo);
-    let topFive = _.take(reverseSortedRepo, 5);
-    user.repos = topFive;
-    console.log (user);
+if (process.argv.length < 3) {
+  console.log ("Please pass username as an argument");
+} else {
+  const userName = process.argv[2];
+  
+  service.getUserInfo(userName, (user: User) => {
+    service.getRepos(userName, (repos: Repo[]) => {
+      let sortedRepo = _.sortBy(repos, (repo: Repo) => repo.size);
+      let reverseSortedRepo = _.reverse(sortedRepo);
+      let topFive = _.take(reverseSortedRepo, 5);
+      user.repos = topFive;
+      console.log (user);
+    });
   });
-});
+}
 
 
