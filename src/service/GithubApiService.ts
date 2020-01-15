c9f356a5a3b4740c531aa30f9acd0037d7ecae8f
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-01-15 10:29:28 +0530

    made GithubApiService class to get user info

---
+++ src/service/GithubApiService.ts
@@ -0,0 +1,17 @@
+import * as request from "request";
+
+export class GithubApiService {
+  getUserInfo (userName: string) {
+    let options: any = {
+      headers: {
+        "User-Agent": "request"
+      }
+    }
+    request.get("https://api.github.com/users" + userName, options, 
+                (error: any, response: any, body: any) => {
+                  console.log(body);
+                });
+  }
+
+  getRepos() {}
+}
